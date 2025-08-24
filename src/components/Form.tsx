84c2355802ea7a8730db466ef0dab3102206eb32
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2025-08-24 20:03:40 +0200

    Add Form component with category selection and activity input handling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,121 @@
-const categories = [
-  { id: 1, name: "Comida" },
-  { id: 2, name: "Ejercicio" },
-];
+import {
+  useState,
+  useEffect,
+  type ChangeEvent,
+  type Dispatch,
+  type FormEvent,
+} from "react";
+import { v4 as uuidv4 } from "uuid";
+import { categories } from "../data/categories";
+import type { Activity } from "../types";
+import type {
+  ActivityActions,
+  ActivityState,
+} from "../reducers/activityReducer";
 
-export default function Form() {
-  return <div>Form</div>;
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>;
+  state: ActivityState;
+};
+
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0,
+};
+
+export default function Form({ dispatch, state }: FormProps) {
+  const [activity, setActivity] = useState<Activity>(initialState);
+
+  useEffect(() => {
+    if (state.activeId) {
+      //console.log(state);
+      const selectedActivity = state.activities.filter(
+        (item) => item.id === state.activeId
+      )[0];
+      setActivity(selectedActivity);
+    }
+  }, [state.activeId]);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
+
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
+    });
+  };
+
+  const isValidActivity = () => {
+    const { name, calories } = activity;
+    return name.trim() !== "" && calories > 0;
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submit ...", e);
+    dispatch({ type: "save-activity", payload: { newActivity: activity } });
+    setActivity({ ...initialState, id: uuidv4() });
+  };
+
+  return (
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">
+          Category:
+        </label>
+        <select
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+          value={activity.category}
+          onChange={handleChange}
+        >
+          {categories.map((category) => (
+            <option key={`category-${category.id}`} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="name" className="font-bold">
+          Activity:
+        </label>
+        <input
+          type="text"
+          id="name"
+          placeholder="Add your activity"
+          className="border border-slate-300 p-2 rounded-lg"
+          value={activity.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">
+          Calories:
+        </label>
+        <input
+          type="number"
+          id="calories"
+          placeholder="How many calories?"
+          className="border border-slate-300 p-2 rounded-lg"
+          value={activity.calories}
+          min={0}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        value={activity.category === 1 ? "Save a food" : "Save a sport"}
+        className="w-full p-2 font-bold uppercase text-white bg-gray-800 transition-colors hover:bg-slate-900 cursor-pointer disabled:opacity-10"
+        disabled={!isValidActivity()}
+      />
+    </form>
+  );
 }
